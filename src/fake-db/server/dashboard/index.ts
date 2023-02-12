e92779644667b9fa0733094d348b4798c82f65d8
Dan Do <dan.do@devbase.us>
2023-02-13 02:06:10 +0700

    update

--- src/fake-db/server/dashboard/index.ts
+++ src/fake-db/server/dashboard/index.ts
@@ -1,8 +1,17 @@
 import Mock from "fake-db/mock";
+import { cardList } from "./data";
 import { pdpTraffic } from "./pdp-traffic";
 import { productTraffic } from "./product-traffic";
 
 
+Mock.onGet("/api/dashboard-cards").reply(() => {
+  try {
+    return [200, cardList];
+  } catch (err) {
+    console.error(err);
+    return [500, { message: "Internal server error" }];
+  }
+});
 
 Mock.onGet("/api/pdp-traffic").reply(() => {
   try {
