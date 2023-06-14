3d0068f97c74b52231694403cf5daec0eda331fa
Dan Do <dan.do@droppii.com>
2023-06-14 14:35:48 +0700

    update

--- src/utils/reports.ts
+++ src/utils/reports.ts
@@ -1,12 +1,15 @@
+import { sortBy } from 'lodash';
 import { ReportDetailResponse, ReportItem, ReportResponse } from 'types/common';
 import { formatDatetime } from './datetime';
 
 export const formatCmsItem = (listCms: ReportResponse[]): ReportItem[] => {
-  return listCms?.map(i => ({
-    ...i,
-    lastTimeDownload: formatDatetime(i.lastTimeDownload, 'dd/MM/yyyy hh:mm:ss a'),
-    lastTimeRecalculate: formatDatetime(i.lastTimeRecalculate, 'dd/MM/yyyy hh:mm:ss a'),
-  }));
+  const list =
+    listCms?.map(i => ({
+      ...i,
+      lastTimeDownload: formatDatetime(i.lastTimeDownload, 'dd/MM/yyyy hh:mm:ss a'),
+      lastTimeRecalculate: formatDatetime(i.lastTimeRecalculate, 'dd/MM/yyyy hh:mm:ss a'),
+    })) || [];
+  return sortBy(list, i => +i.month);
 };
 
 export const formatCmsDetailItem = (
